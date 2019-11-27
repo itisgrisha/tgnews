02fdeb7d63ff1361a5cf0b192ebac9a13c384adc
itisgrisha <itisgrisha@gmail.com>
2019-11-27 05:58:45 +0300

    add test for task 2

---
+++ utils/test.py
@@ -0,0 +1,18 @@
+import json
+import unittest
+from subprocess import run, PIPE, STDOUT
+
+
+class CheckEqualScores(unittest.TestCase):
+    def test_news_det(self):
+        with open("tests/news_det/gt.json", 'r') as f:
+            gt_dict = json.load(f)
+        proc = run(["./build/tgnews", "news_test", "/eee/tgnews/data/20191101/00/"], stdout=PIPE, text=True)
+        prod_dict = json.loads(proc.stdout)
+        for k in prod_dict:
+            self.assertAlmostEqual(prod_dict[k], gt_dict[k], delta=1e-6, msg=f"path={k}")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
