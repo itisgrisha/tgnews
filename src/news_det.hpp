b6b13a260c430b0380656aff033267744d344030
itisgrisha <itisgrisha@gmail.com>
2019-11-27 05:56:52 +0300

    add det scores

--- src/news_det.hpp
+++ src/news_det.hpp
@@ -10,26 +10,35 @@
 #include "features.hpp"
 
 
+std::vector<double> GenerateNewsDetFeatures(const DocFeatures& features) {
+    auto title_upostags = features.Get("title:upostags");
+    auto text_upostags = features.Get("text:upostags");
+    auto title_feats = features.Get("title:feats");
+    auto text_feats = features.Get("text:feats");
+    AddVecMean(&title_feats, text_feats);
+    title_upostags.insert(title_upostags.end(), text_upostags.begin(), text_upostags.end());
+    title_upostags.insert(title_upostags.end(), title_feats.begin(), title_feats.end());
+    DivVec(&title_upostags, 3);
+    return title_upostags;
+}
+
+
 void DetectNewsTask(std::vector<DocFeatures>* documents,
                     const LogisticRegression& ru_model,
                     const LogisticRegression& en_model,
                     size_t start,
                     size_t end) {
-    std::vector<std::string> features_names = {
-        "title:upostags",
-        "text:upostags",
-        "title:feats"
-    };
     for (; start < end; ++start) {
         auto& doc = documents->at(start);
         if (doc.lang_ == "ru") {
-            doc.is_news_ = ru_model.Infer(doc.Concat(features_names)) > 0.5;
+            doc.is_news_score = ru_model.Infer(GenerateNewsDetFeatures(doc));
+            doc.is_news_ = doc.is_news_score > 0.5;
         } else if (doc.lang_ == "en") {
-            doc.is_news_ = en_model.Infer(doc.Concat(features_names)) > 0.5;
+            doc.is_news_score = en_model.Infer(GenerateNewsDetFeatures(doc));
+            doc.is_news_ = doc.is_news_score > 0.5;
         }
     }
 }
-
 
 
 void DetectNews(std::vector<DocFeatures>* documents) {
