eb1346e4db38f1ace1d954bff082cfb81f8ebc49
itisgrisha <itisgrisha@gmail.com>
2019-12-01 17:58:30 +0300

    add separate lemmas for ru and en

--- src/features.hpp
+++ src/features.hpp
@@ -30,16 +30,17 @@ using pReader = std::unique_ptr<udpipe::input_format>;
 class FeaturesGenerator {
 public:
     FeaturesGenerator() {
-        auto prefix = fs::path("models");
+        auto models_prefix = fs::path("models");
+        auto meta_prefix = fs::path("meta");
         for (const auto& lang : kLangs) {
             auto model_name = std::string(lang).append(".udpipe");
-            auto model_path = (prefix / lang / model_name).string();
+            auto model_path = (models_prefix / lang / model_name).string();
             models_[lang].reset(udpipe::model::load(model_path.c_str()));
             readers_[lang].reset(models_[lang]->new_tokenizer("tokenizer")->new_generic_tokenizer_input_format());
+            lang2lemmas_dict_[lang] = ReadLemmas((meta_prefix / lang / "Lemmas.txt"));
         }
         upostags_dict_ = ReadFeaturesNames("meta/Upostags.txt");
         feats_dict_ = ReadFeaturesNames("meta/Feats.txt");
-        lemmas_dict_ = ReadLemmas("meta/Lemmas.txt");
     }
 
     DocFeatures GenerateFeatures(const HTMLDocument& doc, int sentences_count) {
@@ -52,15 +53,16 @@ public:
         auto lang = doc.GetMeta("lang");
         auto& reader = readers_.at(lang);
         auto& model = models_.at(lang);
+        auto& lemmas_dict = lang2lemmas_dict_.at(lang);
 
         if (sentences_count < 0) {
             sentences_count = 1'000'000;
         }
 
         UpdateUDPipe(&features, model, reader, doc.GetText(),
-            {"text:upostags", "text:feats", "text"}, sentences_count);
+            {"text:upostags", "text:feats", "text"}, lemmas_dict, sentences_count);
         UpdateUDPipe(&features, model, reader, doc.GetMeta("og:title"),
-            {"title:upostags", "title:feats", "title"}, sentences_count);
+            {"title:upostags", "title:feats", "title"}, lemmas_dict, sentences_count);
 
         return features;
     }
@@ -106,6 +108,8 @@ private:
                       pReader& reader,
                       const std::string& input_text,
                       std::vector<std::string> features_names,
+                      const std::unordered_map<std::string, std::shared_ptr<BOWDict>>& lemmas_dict,
+
                       int sentences_count) {
         //std::cout << "BEFORE\n" << input_text << std::endl;
         auto text = CleanText(input_text);
@@ -119,7 +123,7 @@ private:
         reader->set_text(text.c_str());
         auto& upostags_bow = features->bows_.at(features_names[0]);
         std::unordered_set<std::string> lemma_upostags = {"ADJ", "INTJ", "NOUN", "PROPN", "VERB"};
-        auto lemmas_dict = lemmas_dict_.at(features_names[2]);
+        auto features_lemmas_dict = lemmas_dict.at(features_names[2]);
         for (int total_sentences = 0;
              (total_sentences < sentences_count) && reader->next_sentence(sentence_, error_message_);
              ++total_sentences) {
@@ -127,7 +131,7 @@ private:
             for (auto word = sentence_.words.begin()+1; word != sentence_.words.end(); ++word) {
                 upostags_bow.Update(word->upostag);
                 if (lemma_upostags.find(word->upostag) != lemma_upostags.end()) {
-                    UpdateLemma(features, word->lemma, lemmas_dict, features_names[2]);
+                    UpdateLemma(features, word->lemma, features_lemmas_dict, features_names[2]);
                 }
                 UpdateFeats(features, word->feats, features_names[1]);
             }
@@ -141,7 +145,7 @@ private:
 
     std::shared_ptr<BOWDict> upostags_dict_;
     std::shared_ptr<BOWDict> feats_dict_;
-    std::unordered_map<std::string, std::shared_ptr<BOWDict>> lemmas_dict_;
+    std::unordered_map<std::string, std::unordered_map<std::string, std::shared_ptr<BOWDict>>> lang2lemmas_dict_;
     std::unordered_set<std::string> categories_;
 
     static constexpr std::initializer_list kLangs = {"ru", "en"};
