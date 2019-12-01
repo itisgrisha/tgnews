d97234304dbd9cd792855d2799eb990c7c20458c
itisgrisha <itisgrisha@gmail.com>
2019-12-01 23:48:14 +0300

    update news_det models: use ru+en

--- src/news_det.hpp
+++ src/news_det.hpp
@@ -12,13 +12,15 @@
 
 
 std::vector<double> GenerateNewsDetFeatures(const DocFeatures& features) {
-    auto title_upostags = features.Get("title:upostags");
-    //auto text_upostags = features.Get("text:upostags");
-    //auto text_feats = features.Get("text:feats");
+    //auto title_upostags = features.Get("title:upostags");
+    auto text_upostags = features.Get("text:upostags");
+    auto text_feats = features.Get("text:feats");
+    text_upostags.insert(text_upostags.end(), text_feats.begin(), text_feats.end());
     //title_upostags.insert(title_upostags.end(), text_upostags.begin(), text_upostags.end());
     //title_upostags.insert(title_upostags.end(), text_feats.begin(), text_feats.end());
     //DivVec(&title_upostags, 3);
-    return title_upostags;
+    DivVec(&text_upostags, 2);
+    return text_upostags;
 }
 
 
